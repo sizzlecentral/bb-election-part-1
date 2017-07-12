de6a46ad8d87fa48ac56c378ab1426f4bd8b2804
Don Cooper <don_cooper@hotmail.com>
2017-07-12 12:04:12 -0400

    Exercise complete

--- election.js
+++ election.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  var candidateList = document.querySelector('#candidates');
 
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(responseData) {
+    for (var i = 0; i < responseData.candidates.length; i++) {
+      var item = document.createElement('li');
+      item.innerHTML = 'Name: ' + responseData.candidates[i].name + ' Votes: ' + responseData.candidates[i].votes;
+      candidateList.appendChild(item);
+    }
+  });
 });
